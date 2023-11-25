35da6c051fa52d5915597af3fc9c9dbe3c047c3a
Grace Nweke <lagra2001@gmail.com>
2023-11-24 20:22:51 -0800

    solving for configuration

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# install flask from pip3
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+# a manifest that kills a process
+exec { 'killmenow':
+    command => '/usr/bin/pkill -f killmenow',
+}
